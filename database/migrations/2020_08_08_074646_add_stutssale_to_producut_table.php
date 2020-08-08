4933b37ff26ab8bb6ed25cee621ff62392e75db9
Pscar <52275689+Pscar@users.noreply.github.com>
2020-08-08 07:51:53 +0700

    update

--- database/migrations/2020_08_08_074646_add_stutssale_to_producut_table.php
+++ database/migrations/2020_08_08_074646_add_stutssale_to_producut_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddStutssaleToProductTable extends Migration
+class AddStutssaleToProducutTable extends Migration
 {
     /**
      * Run the migrations.
